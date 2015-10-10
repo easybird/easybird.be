59d4ef54572ee32dd3e6e858139d8325b783dc08
easybird <easybird85@gmail.com>
2015-10-10 17:47:54 +0200

    created a first version of the new website

--- app/js/init.js
+++ app/js/init.js
@@ -4,5 +4,9 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+      $(document).ready(function(){
+          $('.scrollspy').scrollSpy();
+      });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
