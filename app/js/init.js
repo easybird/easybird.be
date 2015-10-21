3e8d8c4825d0d64d213fd901352bd50f7dcdfc53
easybird <easybird85@gmail.com>
2015-10-21 22:06:20 +0200

    introduced multi language: dutch

--- app/js/init.js
+++ app/js/init.js
@@ -1,3 +1,23 @@
+function main() {
+    var locale = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
+    changeLocale(locale);
+}
+
+function changeLocale(locale) {
+    if (!locale) {
+        locale = 'en';
+    }
+    arb.setResourceSelector(locale);
+
+    // JS localization
+    var r$ = arb.getResource("translations");
+
+    // This should appear after all the translatable HTML content
+    arb.localizeHtml();
+}
+
+main();
+
 (function($){
   $(function(){
 
