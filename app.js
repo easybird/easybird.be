d0a06ddf51b680ff8e4f0fb0b2752e8c105eaae0
easybird <easybird85@gmail.com>
2016-02-23 23:55:04 +0100

    add disqus

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes');
 var languageDetector = require('./middleware/languageDetector');
+var locals = require("./helpers/locals");
 
 languageDetector.initialise({
     locales:['en', 'nl']
@@ -18,6 +19,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.locals.layout = staticData.getConfig().layout;
+
+app.use(function (req, res, next) {
+    locals.addSiteLocals(res, {
+        url: 'easybird.be'
+    });
+    next();
+});
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
